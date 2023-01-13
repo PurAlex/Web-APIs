db016925ef071f267bf177977fc19f167c33da53
Alexander <nicky-nico-1998@outlook.com>
2023-01-13 15:48:34 +0000

    Starter Code

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,6 @@
+console.log("logic");
+
+
+for (var i = 0; i < myQuestions.length; i++) {
+    (myQuestions[i].question);
+}
---
+++ starter/assets/js/questions.js
@@ -0,0 +1,22 @@
+console.log("questions");
+
+var myQuestions = [
+    {
+        question: "Array in JavaScript can be used to store",
+        answers: [
+            "booleans",
+            "All of the above",
+            "Other arrays",
+            "numbers or strings"
+        ]
+    },
+    {
+        question: "Commonly used data types Do Not include",
+        answers: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+        ]
+    },
+]
