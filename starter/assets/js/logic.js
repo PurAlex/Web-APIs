94e80a67af1a2601e8eda599e0d894e3e3a6736e
Alexander <nicky-nico-1998@outlook.com>
2023-01-13 16:35:04 +0000

    Questions added and addEventListener started

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,6 +1,27 @@
 console.log("logic");
 
+var startQuiz = document.querySelector("#start");
+var timer = document.querySelector("#time");
+var secondsLeft = 75;
 
-for (var i = 0; i < myQuestions.length; i++) {
-    (myQuestions[i].question);
-}
+// When Start Quiz button is clicked, questions load and timer is on.
+startQuiz.addEventListener("click", function () {
+    for (var i = 0; i < myQuestions.length; i++) {
+        // console.log(myQuestions[i].question);
+        myQuestions.textContent = "";
+    };
+
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timer.textContent = secondsLeft;
+
+        if (secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // Calls function of scores
+            scores();
+        }
+    }, 1000);
+
+
+});
--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,5 +1,5 @@
 console.log("questions");
-
+// Questions and answers 
 var myQuestions = [
     {
         question: "Array in JavaScript can be used to store",
@@ -17,6 +17,42 @@ var myQuestions = [
             "booleans",
             "alerts",
             "numbers"
+        ]
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        answers: [
+            "The <body> section",
+            "The <head> section",
+            "Both the <head> section and the <body> section are correct",
+            "Outside <html>"
+        ]
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: [
+            "function:myFunction()",
+            "function = myFunction()",
+            "function.myFunction()",
+            "function myFunction"
+        ]
+    },
+    {
+        question: "How does a FOR loop start?",
+        answers: [
+            "for (i = 0; i < 5; i++",
+            "for (i < 5; i++",
+            "for i = 1 to 5",
+            "for (i = 0; i < 5"
+        ]
+    },
+    {
+        question: "How can you add a comment in JavaScript?",
+        answers: [
+            "'This is a comment",
+            "//This is a comment",
+            "<!--This is a comment-->",
+            "||This is a comment||"
         ]
     },
 ]
