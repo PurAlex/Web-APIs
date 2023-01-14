8b8c426d7378403f584db506fd3af6f0ef4bca94
Alexander <nicky-nico-1998@outlook.com>
2023-01-14 18:37:00 +0000

    Time function added

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -2,15 +2,39 @@ console.log("logic");
 
 var startQuiz = document.querySelector("#start");
 var timer = document.querySelector("#time");
+var startedScreen = document.querySelector("#start-screen");
+
+
+var questionsEl = document.querySelector("#question-title");
+
+
 var secondsLeft = 75;
 
 // When Start Quiz button is clicked, questions load and timer is on.
 startQuiz.addEventListener("click", function () {
     for (var i = 0; i < myQuestions.length; i++) {
         // console.log(myQuestions[i].question);
-        myQuestions.textContent = "";
+        startedScreen.textContent = "";
+
+        //change class "hide" to "visible"
+        document.getElementById("questions").className = "visible";
+        // questionsEl.textContent = myQuestions; 
+
+
+
+
+
+
+
     };
 
+    startTime();
+
+
+});
+
+
+function startTime() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
         timer.textContent = secondsLeft;
@@ -22,6 +46,4 @@ startQuiz.addEventListener("click", function () {
             scores();
         }
     }, 1000);
-
-
-});
+};
